f0571fd34c22b2fcf7e70675e92870db2306f1f6
Даниил Герасин <gerasin.dan@yandex.ru>
2023-09-19 02:50:46 +0300

    Revert "Изменил метод ReadLineFromTxt на GetWordsFromFile в классе FileReader, теперь возвращает последовательность из слов, заместо строк"
    This reverts commit 5326f0f6d6f34703edf3e5edd3ee1605b7177659.

--- TripletFinder/TripletFinder.cs
+++ TripletFinder/TripletFinder.cs
@@ -12,24 +12,21 @@ namespace TripletFinder
         /// <returns>Последовательность из 10 самых часто встречающихся триплетов в порядке убывания частоты повторений.</returns>
         public IEnumerable<KeyValuePair<string, int>> FindTriplet(string path)
         {
+            Regex regex = new(@"(?=(\p{L}{3}))");
             ConcurrentDictionary<string, int> triplets = new();
             try
             {
                 FileReader file = new(path);
 
                 Parallel.ForEach(
-                    file.GetWordsFromFile(),
+                    file.ReadLineFromTxt(),
                     new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
-                    word =>
+                    line =>
                     {
-                        for (int i = 0; i < word.Length - 2; i++)
+                        var matches = regex.Matches(line);
+                        foreach (var match in matches.Cast<Match>())
                         {
-                            var triplet = word.Substring(i, 3);
-                            
-                            if (triplet.All(char.IsLetter))
-                            {
-                                triplets.AddOrUpdate(triplet, 1, (_, count) => count + 1);
-                            }
+                            triplets.AddOrUpdate(match.Groups[1].Value, 1, (_, count) => count + 1);
                         }
                     });
             }
@@ -41,6 +38,7 @@ namespace TripletFinder
             var results = triplets.OrderByDescending(x => x.Value).Take(10);
 
             return results;
+
         }
         /// <summary>
         ///  Выводит на консоль информацию о триплетах и количестве их повторений.
